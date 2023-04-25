beb31890c0eb68f9209b7e6c0ec1af31072d11aa
Etai Levi <etai3000@gmail.com>
2023-04-25 18:28:15 +0300

    Support rendering text on canvas

--- js/controller.js
+++ js/controller.js
@@ -9,41 +9,19 @@ renderMeme()
 function renderMeme() {
     gElCanvas = document.querySelector('#my-canvas')
     gCtx = gElCanvas.getContext('2d')
-
-    drawImg2()
-    gCtx.save()
-    renderText()
-}
-
-
-
-function drawImg2() {
     const elImg = new Image() // Create a new html img element
     elImg.src = 'meme-imgs (square)/1.jpg' // Send a network req to get that image, define the img src
     console.log('elImg:', elImg)
     // When the image ready draw it on the canvas
     elImg.onload = () => {
         gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+        gCtx.font = '25px poppins'
+        gCtx.fillStyle = 'green'
+        gCtx.strokeStyle = 'black'
+        gCtx.fillText('hello world!', gElCanvas.width / 3, gElCanvas.height / 2)
+        gCtx.strokeText('hello world!', gElCanvas.width / 3, gElCanvas.height / 2)
     }
 }
-
-function renderText() {
-    gCtx.font = '30px Arial'
-    gCtx.strokeStyle = 'green'
-    gCtx.strokeText('Saving the context', 10, 50)
-
-    gCtx.save() // Saves the current drawing style state using a stack.
-
-    gCtx.font = '25px sans serif'
-    gCtx.strokeStyle = 'black'
-    gCtx.strokeText('Switching to something else', 10, 100)
-
-    gCtx.restore() // Restores the drawing style state to the last element on the 'state stack' saved by save().
-
-    gCtx.strokeText('Back to previous context', 10, 150)
-}
-
-
 
 function downloadImg(elLink) {
     // Gets the canvas content and convert it to base64 data URL that can be save as an image
